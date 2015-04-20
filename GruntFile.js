c5e12f0a7745c8ba5242584da3e49015ab52db71
= <=>
2015-04-20 17:50:58 +0200

    Esqueleto inicial para jenkins.

---
+++ GruntFile.js
@@ -0,0 +1,27 @@
+/**
+ * Created by vladimiro.agudo on 20/04/2015.
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/lib/*.js',
+                dest: 'app/topgank.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
---
+++ src/lib/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vladimiro.agudo on 20/04/2015.
+ */
